2d8ad8313ff938ef68c1a52a6166d7a3ac72610b
fore90009-star <fore90009@gmail.com>
2026-07-04 13:35:39 +0900

    Update script.js

--- site2/script.js
+++ site2/script.js
@@ -1,5 +1,5 @@
 // 접속 비밀번호
-const PASSWORD = "1212";
+const PASSWORD = "0125";
 
 // 이동할 주소
 const REDIRECT_URL = "https://map.naver.com/p/search/%EB%8C%80%EC%A0%84%20%EC%84%B8%ED%95%98%EC%B9%98%EA%B3%BC/place/34347279?placePath=%2Fhome%3FabtExp%3DNEW-PLACE-SEARCH%3A5%26bk_query%3D%EB%8C%80%EC%A0%84%20%EC%84%B8%ED%95%98%EC%B9%98%EA%B3%BC%26entry%3Dpll%26from%3Dnx%26fromNxList%3Dtrue%26from%3Dmap%26fromPanelNum%3D2%26timestamp%3D202607041323%26locale%3Dko%26svcName%3Dmap_pcv5%26searchText%3D%EB%8C%80%EC%A0%84%20%EC%84%B8%ED%95%98%EC%B9%98%EA%B3%BC&bk_query=%EB%8C%80%EC%A0%84%20%EC%84%B8%ED%95%98%EC%B9%98%EA%B3%BC&entry=pll&from=nx&fromNxList=true&searchType=place&c=15.00,0,0,0,dh";
